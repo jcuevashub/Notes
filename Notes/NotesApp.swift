31add4d402fd333cda3562154a204b3dcd3a77a5
Jackson Cuevas <jackson.cuevas@hotmail.com>
2025-01-10 07:32:03 -0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Jackson Cuevas on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
